ad7323e27daa1d39cd2a73fbe85fcf7f92f52e6a
vinnubhai <vinaydhp1@gmail.com>
2023-09-23 21:29:25 +0530

    i am make my first programme of inheritance.

---
+++ code/src/main/java/com/example/code/box.java
@@ -0,0 +1,22 @@
+package com.example.code;
+
+public class box extends rectangle {
+private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ code/src/main/java/com/example/code/rectangle.java
@@ -0,0 +1,26 @@
+package com.example.code;
+
+import java.util.Scanner;
+
+
+public class rectangle {
+
+    private int lenth;
+    private int width;
+
+    public int getLenth() {
+        return lenth;
+    }
+
+    public void setLenth(int lenth) {
+        this.lenth = lenth;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
